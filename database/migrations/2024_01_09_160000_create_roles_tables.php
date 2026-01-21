99831575ae72e05c6f73e2a045f48ecf7e0e85e9
Dylan Vianen <dylchelvia@gmail.com>
2026-01-21 13:43:38 +0100

    Fix formatting and comments in roles migration for improved readability

--- database/migrations/2024_01_09_160000_create_roles_tables.php
+++ database/migrations/2024_01_09_160000_create_roles_tables.php
@@ -13,7 +13,7 @@ return new class extends Migration
             $table->string('Naam', 100);
             $table->boolean('IsActief')->default(true);
             $table->string('Opmerking', 255)->nullable();
-            
+
             // Custom timestamps
             $table->timestamp('DatumAangemaakt')->useCurrent();
             $table->timestamp('DatumGewijzigd')->useCurrent()->useCurrentOnUpdate();
@@ -29,17 +29,17 @@ return new class extends Migration
             // Custom timestamps
             $table->timestamp('DatumAangemaakt')->useCurrent();
             $table->timestamp('DatumGewijzigd')->useCurrent()->useCurrentOnUpdate();
-            
+
             // Foreign keys
             // Note: users table usually has 'id' as bigIncrements or unsignedBigInteger
-            // But the script says INT UNSIGNED for users.id. 
+            // But the script says INT UNSIGNED for users.id.
             // In the create_users_table migration standard Laravel uses id() which is bigIncrements.
             // Let's assume standard Laravel users.id for now, but referenced as GebruikerId.
             // If users.id is BigInt, GebruikerId should be too?
             // The script says users.id is INT UNSIGNED.
             // The default create_users_table uses $table->id() which is bigIncrements.
             // To be safe with SQLite and typical Laravel setups, I'll match the type.
-            // Usually we just don't add the foreign key constraint in SQLite tests if getting types right is hard, 
+            // Usually we just don't add the foreign key constraint in SQLite tests if getting types right is hard,
             // but let's try to be correct.
         });
     }

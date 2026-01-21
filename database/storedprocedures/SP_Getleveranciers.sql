23ce97a80e2b126e829605d3d6179e4158d5370b
Dylan Vianen <dylchelvia@gmail.com>
2026-01-21 13:44:09 +0100

    Add stored procedure SP_Getleveranciers to retrieve suppliers with contact details, filtered by supplier type

--- database/storedprocedures/SP_Getleveranciers.sql
+++ database/storedprocedures/SP_Getleveranciers.sql
@@ -2,6 +2,37 @@ USE voedselbank2;
 
 DROP PROCEDURE IF EXISTS SP_Getleveranciers;
 
-DELEMITER $$
+DELIMITER $$
 
+-- =============================================
+-- Author:      Dylan
+-- Create date: 21-01-2026
+-- Description: Haalt leveranciers op, inclusief contactgegevens
+--              Optioneel gefilterd op LeverancierType
+-- =============================================
+CREATE PROCEDURE SP_Getleveranciers(
+    IN p_LeverancierType VARCHAR(50)
+)
+BEGIN
+    SELECT
+        l.Id,
+        l.Naam,
+        l.ContactPersoon,
+        l.LeverancierNummer,
+        l.LeverancierType,
+        c.Email,
+        c.Mobiel
+    FROM Leverancier l
+    LEFT JOIN ContactPerLeverancier cpl ON l.Id = cpl.LeverancierId AND cpl.IsActief = 1
+    LEFT JOIN Contact c ON cpl.ContactId = c.Id AND c.IsActief = 1
+    WHERE l.IsActief = 1
+    AND (p_LeverancierType = '' OR l.LeverancierType = p_LeverancierType)
+    ORDER BY l.Naam;
+END$$
+
+DELIMITER ;
+
+-- Test calls
+-- call SP_Getleveranciers('');
+-- call SP_Getleveranciers('Bedrijf');
 

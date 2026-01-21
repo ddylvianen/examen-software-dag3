8fcde445d37650c470acb8c87fb55d01e74bf324
Dylan Vianen <dylchelvia@gmail.com>
2026-01-21 13:44:03 +0100

    Add stored procedure SP_Getleverancierbyid to retrieve supplier details by ID

---
+++ database/storedprocedures/SP_GetLeverancierbyid.sql
@@ -0,0 +1,24 @@
+USE voedselbank2;
+
+DROP PROCEDURE IF EXISTS SP_Getleverancierbyid;
+-- naam, soort allergie, barcode, houdbaarheidsdatum, leveranciernaam, leveranciernummer, leveranciertype
+DELIMITER $$
+-- =============================================
+-- Author:      Dylan
+-- Create date: 21-01-2026
+-- Description: Haalt een specifieke leverancier op via ID
+-- =============================================
+CREATE PROCEDURE SP_Getleverancierbyid(
+    IN p_leverancierid INT
+)
+BEGIN
+    SELECT
+        l.Naam AS LeverancierNaam,
+        l.LeverancierNummer,
+        l.LeverancierType
+    FROM Leverancier l
+    WHERE l.id = p_leverancierid;
+END$$
+
+
+CALL SP_Getleverancierbyid(1);
